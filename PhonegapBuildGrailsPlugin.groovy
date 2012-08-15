2ee79ffe699e12cdbd319d24ff693eb8181f4e28
corinne <corinne@corinne.(none)>
2012-08-15 21:47:52 +0200

    cleaning

--- PhonegapBuildGrailsPlugin.groovy
+++ PhonegapBuildGrailsPlugin.groovy
@@ -1,71 +1,48 @@
+/* Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ *
+ * @author <a href='mailto:th33musk3t33rs@gmail.com'>3.musket33rs</a>
+ *
+ * @since 0.1
+ */
+
 class PhonegapBuildGrailsPlugin {
-    // the plugin version
-    def version = "0.1"
-    // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.1 > *"
-    // the other plugins this plugin depends on
+    def version = "0. version or versions of Grails the plugin is designed for
+    def grailsVersion = "2.0 > *"
     def dependsOn = [:]
-    // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp"
     ]
-
-    // TODO Fill in these fields
     def title = "Phonegap Build Plugin" // Headline display name of the plugin
-    def author = "Your name"
-    def authorEmail = ""
+    def author = '3.musket33rs'
+    def authorEmail = 'th33musk3t33rs@gmail.com'
+    def organization = [name: '3.musket33rs', url: 'http://3musket33rs.github.com/']
+    def developers = [
+      [ name: "Aramis alias Sebastien Blanc", email: "scm.blanc@gmail.com"],
+      [ name: "Athos alias Corinne Krych", email: "corinnekrych@gmail.com" ],
+      [ name: "Porthos alias Fabrice Matrat", email: "fabricematrat@gmail.com" ]
+    ]
+
     def description = '''\
-Brief summary/description of the plugin.
+PhoneGap build which allow you to build all packages available on any phone for your mobile application.
 '''
 
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/phonegap-build"
-
-    // Extra (optional) plugin metadata
-
-    // License: one of 'APACHE', 'GPL2', 'GPL3'
-//    def license = "APACHE"
-
-    // Details of company behind the plugin (if there is one)
-//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
-
-    // Any additional developers beyond the author specified above.
-//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
-
-    // Location of the plugin's issue tracker.
-//    def issueManagement = [ system: "JIRA", url: "http://jira.grails.org/browse/GPMYPLUGIN" ]
-
-    // Online location of the plugin's browseable source code.
-//    def scm = [ url: "http://svn.codehaus.org/grails-plugins/" ]
-
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before
-    }
-
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
-
-    def onShutdown = { event ->
-        // TODO Implement code that is executed when the application shuts down (optional)
-    }
+  def documentation = 'http://3musket33rs.github.com/phonegapbuild/'
+  def license = 'APACHE'
+  def issueManagement = [system: 'GitHub', url: 'https://github.com/3musket33rs/phonegapbuild/issues']
+  def scm = [url: 'https://github.com/3musket33rs/phonegapbuild']
 }
